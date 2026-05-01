98fa56558b5f91831475c4c4e23412fadb480ec3
angiebow <pelangimasita18@gmail.com>
2026-05-01 17:17:27 +0700

    feat: mobile app placeholder

---
+++ prometheus-app/prometheus-app/prometheus_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  prometheus_appApp.swift
+//  prometheus-app
+//
+//  Created by Pelangi Masita Wati on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct prometheus_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
